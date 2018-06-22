1f6e31e7b8ec0eda4627ec0718a22f5229c58491
xujian <xujian@fit2cloud.com>
2018-06-22 16:49:02 +0800

    更新

--- src/main/java/com/fit2cloud/sdk/model/ApplicationDeployPolicyType.java
+++
@@ -1,6 +0,0 @@
-package com.fit2cloud.sdk.model;
-public class ApplicationDeployPolicyType {
-	public static final String ALL_AT_ONCE = "allAtOnce";
-	public static final String HALF_AT_A_TIME = "halfAtATime";
-	public static final String ONE_AT_A_TIME = "oneAtATime";
-}
